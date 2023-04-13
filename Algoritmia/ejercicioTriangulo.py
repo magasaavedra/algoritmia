04f4bea0b0500c2dce0a0868c2c8c45808791d7e
Magali Saavedra <82746129+magasaavedra@users.noreply.github.com>
2023-04-13 14:25:13 -0300

    Update ejercicioTriangulo.py

--- Algoritmia/ejercicioTriangulo.py
+++ Algoritmia/ejercicioTriangulo.py
@@ -10,11 +10,11 @@ c = int(input("Ingresar lado "))
 #Primero comparo los lados para verificar que sean todos iguales o que la suma de dos lados sea mayor al valor del tercero. Luego clasifico. 
 if a + b > c and a + c > b and b + c > a:
     if a == b == c:
-        print("Los valores ingresados forman un triángulo escaleno")
+        print("Los valores ingresados forman un triángulo equilatero")
     elif a == b != c or b == c != a or a == c != b:
         print("Los valores ingresados forman un triángulo isoseles")
     else:
-        print("Los valores ingresados forman un triángulo equilatero")
+        print("Los valores ingresados forman un triángulo escaleno")
 else:
     print("Los valores ingresados no forman un triángulo")
         
